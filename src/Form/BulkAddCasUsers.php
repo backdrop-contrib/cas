cc6e84ac58c20657c0afd35ea5051073e94678fe
bkosborne <bkosborne@788032.no-reply.drupal.org>
2020-06-14 13:52:10 -0400

    Issue #3106985 by bkosborne, segovia94: When bulk-adding CAS users, email is malformed

--- src/Form/BulkAddCasUsers.php
+++ src/Form/BulkAddCasUsers.php
@@ -42,6 +42,15 @@ class BulkAddCasUsers extends FormBase {
       '#description' => $this->t('Enter one username per line.'),
     ];
 
+    $form['email_hostname'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Email address'),
+      '#description' => $this->t("The email domain name used to combine with the username to form the user's email address. If your user's email address is usually provided via a CAS attribute, that will not work here because CAS attributes are not available."),
+      '#field_prefix' => $this->t('username@'),
+      '#required' => TRUE,
+      '#default_value' => $this->config('cas.settings')->get('user_accounts.email_hostname'),
+    ];
+
     $roles = array_map(['\Drupal\Component\Utility\Html', 'escape'], user_role_names(TRUE));
     $form['roles'] = [
       '#type' => 'checkboxes',
@@ -56,7 +65,7 @@ class BulkAddCasUsers extends FormBase {
 
     $form['extra_info'] = [
       '#prefix' => '<p>',
-      '#markup' => $this->t("Note that because CAS attributes are only available when a user has logged in, any role or field assignment based on attributes will not be available using this form."),
+      '#markup' => $this->t("Note that because CAS attributes are only available when a user authenticates with CAS, any role or field assignment based on attributes will not be available."),
       '#suffix' => '</p>',
     ];
 
@@ -80,13 +89,15 @@ class BulkAddCasUsers extends FormBase {
     $cas_usernames = trim($form_state->getValue('cas_usernames'));
     $cas_usernames = preg_split('/[\n\r|\r|\n]+/', $cas_usernames);
 
+    $email_hostname = trim($form_state->getValue('email_hostname'));
+
     $operations = [];
     foreach ($cas_usernames as $cas_username) {
       $cas_username = trim($cas_username);
       if (!empty($cas_username)) {
         $operations[] = [
           '\Drupal\cas\Form\BulkAddCasUsers::userAdd',
-          [$cas_username, $roles],
+          [$cas_username, $roles, $email_hostname],
         ];
       }
     }
@@ -112,8 +123,10 @@ class BulkAddCasUsers extends FormBase {
    *   An array of roles to assign to the user.
    * @param array $context
    *   The batch context array, passed by reference.
+   * @param string $email_hostname
+   *   The hostname to combine with the username to create the email address.
    */
-  public static function userAdd($cas_username, array $roles, array &$context) {
+  public static function userAdd($cas_username, array $roles, $email_hostname, array &$context) {
     $cas_user_manager = \Drupal::service('cas.user_manager');
 
     // Back out of an account already has this CAS username.
@@ -125,18 +138,8 @@ class BulkAddCasUsers extends FormBase {
 
     $user_properties = [
       'roles' => $roles,
+      'mail' => $cas_username . '@' . $email_hostname,
     ];
-
-    // If the email assignment strategy is based on username and fixed hostname,
-    // then we can provide an email address for the account. If the email is
-    // filled out based on a CAS attribute, there's nothing we can do to fill
-    // it out because we don't have access to CAS attributes without a login
-    // occuring.
-    $cas_settings = \Drupal::config('cas.settings');
-    $email_assignment_strategy = $cas_settings->get('user_accounts.email_assignment_strategy');
-    if ($email_assignment_strategy === CasUserManager::EMAIL_ASSIGNMENT_STANDARD) {
-      $user_properties['mail'] = $cas_username . '@' . $cas_settings->get('user_accounts.email_hostname');
-    }
 
     try {
       /** @var \Drupal\user\UserInterface $user */
